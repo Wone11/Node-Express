ca9ec77cfa4c46b32648854999d5f73f8c65807d
solomongr <solomongr@awashbank.com>
2022-09-29 21:44:42 +0300

    Dosomething thing function  with 100 milesecond interval added

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const data = [1,2,3,4,5,6,7,8]
 for(let item = 0; item < data.length ; item ++){
     console.log(data[item]**2)
 }
+
+
+function Dosomething(){
+    console.log(__dirname)
+    setInterval(() => {
+        console.log(__filename)
+    }, 100)
+}
+
+//Call a function Here
+Dosomething()

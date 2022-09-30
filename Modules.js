0dd3f387e5e7739d2f9690da7b7605d6a0c59764
solomongr <solomongr@awashbank.com>
2022-09-30 23:01:45 +0300

    Modules.js os details display about the os ....

--- Modules.js
+++ Modules.js
@@ -12,6 +12,28 @@ console.log(os.cpus())
 //Platform
 console.log('PlatForm  : ',os.platform())
 
-
+//Info about the Network Interface...
 console.log('Nick INterface  : ',os.networkInterfaces())
 
+
+//Info about the current users
+console.log('User Account Details', os.userInfo())
+
+//Sytem uptime
+
+function System_Up_Time(){
+    console.log(`System up time ${os.uptime() / 3600}   Hours` );
+}
+
+System_Up_Time()
+
+const CurrentOS ={
+    name:os.type(),
+    version:os.version(),
+    architecture:os.arch(),
+    release:os.release(),
+    freeMemory:os.freemem(),
+    totalMemory:os.totalmem()
+}
+
+console.log('OS Information  ', CurrentOS);
